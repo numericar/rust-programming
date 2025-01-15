56da46b77c577d93d59c67e9d622b7da7f30ff9d
Alphanumeric Str <150432237+numericar@users.noreply.github.com>
2025-01-15 13:35:42 +0700

    lecture: immutable mutable

--- src/hello-crabby/src/main.rs
+++ src/hello-crabby/src/main.rs
@@ -1,3 +1,3 @@
 fn main() {
-    println!("Hello, Rosetta!");
+    println!("Hello, world!");
 }
---
+++ src/immu_mut/src/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    let mut crabby_enegy: i16 = 100;
+    crabby_enegy = 90;
+    println!("crabby energy: {}", crabby_enegy);
+}
